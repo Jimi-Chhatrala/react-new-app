769bb0dfa901a63cf5c4e3d01175db51cc122e7e
Jimi-Chhatrala <90768094+Jimi-Chhatrala@users.noreply.github.com>
2022-08-24 22:09:21 +0530

    Class Component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,46 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Sidebar />
-      <Footer />
-      <App2 />
-    </>
-  );
+// import React from "react";
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello Class Component</h1>;
+//   }
+// }
+import React, { Component } from "react";
+
+// export default class App extends Component {
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello Class Component</h1>
+        <p>This is class component paragraph.</p>;
+      </>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <>
+//       <Header />
+//       <Sidebar />
+//       <Footer />
+//       <App2 />
+//     </>
+//   );
+// }
 
 // export function App2() {
-function App2() {
-  return (
-    <>
-      <h1>App2</h1>
-    </>
-  );
-}
+// function App2() {
+//   return (
+//     <>
+//       <h1>App2</h1>
+//     </>
+//   );
+// }
 
-export { App2 };
-export default App;
+// export { App2 };
+// export default App;
 // export { App, App2 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import New, { App2 } from "./App";
+// import New, { App2 } from "./App";
 // import New from "./App";
-// import App from "./App";
+import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HelloMessage from "./HelloMessage";
 import House from "./House";
@@ -14,10 +14,10 @@ let b = 30;
 const friends = ["Amit", "Jazz", "Kumar", "Roy"];
 root.render(
   <React.StrictMode>
-    <New />
+    {/* <New /> */}
     {/* <House /> */}
-    {/* <App /> */}
-    <App2 />
+    <App />
+    {/* <App2 /> */}
     {/* <HelloMessage name={a} age={b} > */}
     {/* <p>Children Element 1</p> */}
     {/* </HelloMessage> */}
